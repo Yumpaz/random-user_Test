d6feb5f03d86c18a52a9dea9716ffa12882e0840
Miguel <ohyeahs4@hotmail.com>
2023-10-09 22:11:31 -0500

    materialUI modal

--- src/components/itemUser.jsx
+++ src/components/itemUser.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import {
   makeStyles,
@@ -8,6 +8,8 @@ import {
   Typography,
   Box,
   Button,
+  Modal,
+  Paper,
 } from "@material-ui/core";
 import Logo from "./images/logo192.png";
 import { Link } from "react-router-dom";
@@ -31,10 +33,29 @@ const useStyle = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "space-around",
   },
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  confirm: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    height: "20%",
+    width: "20%",
+  },
+  modalActions: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    width: '80%'
+  }
 }));
 
 const ItemUser = (props) => {
   const { name, country, email, telefono, id } = props.user;
+  const [open, setopen] = useState(false);
   const classes = useStyle();
 
   const handleDelete = async () => {
@@ -44,6 +65,10 @@ const ItemUser = (props) => {
     } catch (error) {
       console.log("error");
     }
+  };
+
+  const handleModal = () => {
+    setopen(true);
   };
 
   return (
@@ -82,16 +107,29 @@ const ItemUser = (props) => {
             </Button>
           )}
           {props.isDelete && (
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={handleDelete}
-            >
+            <Button variant="contained" color="secondary" onClick={handleModal}>
               Eliminar
             </Button>
           )}
         </Box>
       </CardContent>
+      <Modal className={classes.modal} open={open}>
+        <Paper className={classes.confirm}>
+          <Typography>Esta seguro de eliminar este usuario?</Typography>
+          <Box className={classes.modalActions}> 
+            <Button variant="contained" color="primary" onClick={handleDelete}>
+              Aceptar
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => setopen(false)}
+            >
+              Cancelar
+            </Button>
+          </Box>
+        </Paper>
+      </Modal>
     </Card>
   );
 };
